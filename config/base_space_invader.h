982f2dfdb82f4154c84fa07f97accd525a65f669
Nico Stuhlmueller <n+git@ncxst.de>
2025-03-02 14:39:11 +0100

    fixed hrm

--- config/base_space_invader.h
+++ config/base_space_invader.h
@@ -146,7 +146,7 @@
             // global-quick-tap;
             bindings = <&kp>, <&kp>;
             // opposite side hand keys
-            hold-trigger-key-positions = <4 5 6 7 13 14 15 16 17 21 22 23 24 25 26 27>;
+            hold-trigger-key-positions = <5 6 7 8 9 15 16 17 18 19 23 24 25 26 27 28 29 30 31 32 33>;
         };
         hr: homerow_mods_right {
             compatible = "zmk,behavior-hold-tap";
@@ -159,7 +159,7 @@
             // global-quick-tap;
             bindings = <&kp>, <&kp>;
             // opposite side hand keys
-            hold-trigger-key-positions = <0 1 2 3 8 9 10 11 12 18 19 20 24 25 26 27>;
+            hold-trigger-key-positions = <0 1 2 3 4 10 11 12 13 14 20 21 22 27 28 29 30 31 32 33>;
         };
 
         my_lt: my_layer_taps {
@@ -173,7 +173,7 @@
             // global-quick-tap;
             bindings = <&mo &kp>, <&kp>;
             // non-thumb keys
-            hold-trigger-key-positions = <0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27>;
+            hold-trigger-key-positions = <0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33>;
         };
 
         dotcol: dot_colon {
